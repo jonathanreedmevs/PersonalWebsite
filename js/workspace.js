6c53f5213c0beafafeb6e2c322d84a2132ecd156
jonathan <jrmevs@umd.edu>
2017-08-02 10:31:47 -0400

    made the code more elegant

--- js/workspace.js
+++ js/workspace.js
@@ -6,70 +6,61 @@
 
 
 // var newsapiKEY = 2461bdbe914f4494a27aa8374d7d976a;
-// var breitbartURL = "https://newsapi.org/v1/articles?source=breitbart-news&sortBy=latest&apiKey=2461bdbe914f4494a27aa8374d7d976a";
 
 
-/**
- * The data structure initialized below is here for embedding the timelines
- * into the webpage. Twitter provides the links to this code.
- * They are updated in the function makeRequestAndDisplay
- * when there is the need to specify a data-height so it fits inside the parent element.
- */
-var twitterHTMLCodeMap = new Map();
-twitterHTMLCodeMap.set("cnn", "<a class=\"twitter-timeline\" href=\"https://twitter.com/CNN\">Tweets by CNN</a> <script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>");
-twitterHTMLCodeMap.set("huffpost", "<a class=\"twitter-timeline\" href=\"https://twitter.com/HuffPost\">Tweets by HuffPost</a> <script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>");
 
 var cnnURL = "https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=2461bdbe914f4494a27aa8374d7d976a";
 var huffingtonpostURL = " https://newsapi.org/v1/articles?source=the-huffington-post&sortBy=top&apiKey=2461bdbe914f4494a27aa8374d7d976a";
+var reutersURL = "https://newsapi.org/v1/articles?source=reuters&sortBy=top&apiKey=2461bdbe914f4494a27aa8374d7d976a";
+var breitbartURL = "https://newsapi.org/v1/articles?source=breitbart-news&sortBy=top&apiKey=2461bdbe914f4494a27aa8374d7d976a";
 
-makeRequestAndDisplay("cnn", cnnURL, "#cnn-news-col", "#cnn-twitter-col", "cnn-top-link");
-makeRequestAndDisplay("huffpost", huffingtonpostURL, "#huff-post-news-col", "#huff-post-twitter-col", "huff-post-top-link");
 
-function makeRequestAndDisplay(source, newsSourceURL, newsColID, newsTwitterID, topLinkName) {
-    $.ajax({
-        url: newsSourceURL
-    }).done(function (data) {
-        var picID = "\"" + source + "-pic\"";
-        var article = data.articles[0];
-        var author = article.author;
-        var headline = article.title;
-        var description = article.description;
-        var link = article.url;
-        var imgSource = article.urlToImage;
-        var twitterLinkCode = twitterHTMLCodeMap.get(source);
-        //display the headline
-        $(newsColID).append("<h1>" + headline + "</h1>")
-        //if the author is null, it says written by Null. Nothing will be displayed if that is the case.
-        if(author != null){
-            $(newsColID).append("<h2>Written by: " + author + ".");
-        }
-        //display a description
-        $(newsColID).append("<p>" + description + "</p>");
-        //link to the article itself
-        $(newsColID).append("<a target=\"_blank\" id=\"" + topLinkName + "\"href=" + "\"" + link + "\"" + ">");
-        //for id referencing
-        topLinkName = "#".concat(topLinkName);
-        //Adding text to be displayed with the link
-        $(topLinkName).append("View Article Here");
-        //displaying the article's image
-        $(newsColID).append("<img class=\"news-pic\" id=" + picID + "src=" + imgSource + ">");
-        //for referencing purposes
-        source = "#".concat(source);
-        /**
-         * Here we are getting the height of the parent div element. 
-         * For embedded timelines, twitter offers the option of data-height, to 
-         * specify the height, in px, of the timeline to be displayed.
-         */
-        var height = $(source).height();
-        var dataHeight = " data-height=" + height;
-        //these lines are so I can enter it in the twitter code that is already provided
-        //and stored in our map.
-        var temp = twitterLinkCode.substring(0,2) + dataHeight + twitterLinkCode.substring(2, twitterLinkCode.length + 1);
-        twitterLinkCode = temp;
-        //we don't need to "#" we appended earlier
-        twitterHTMLCodeMap.set(source.slice(1), twitterLinkCode);
-        $(newsTwitterID).append(twitterLinkCode);
+var CNN = new NewsSource("cnn", cnnURL, "#cnn-news-col", "#cnn-twitter-col", 
+            "<a class=\"twitter-timeline\" href=\"https://twitter.com/CNN\">Tweets by CNN</a>" + 
+            "<script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>", 
+            "cnn-top-link");
+
+var HuffingtonPost = new NewsSource("huffpost", huffingtonpostURL, "#huff-post-news-col", "#huff-post-twitter-col",
+            "<a class=\"twitter-timeline\" href=\"https://twitter.com/HuffPost\">Tweets by HuffPost</a>" +
+            "<script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>", 
+            "huff-post-top-link");
+
+var Reuters = new NewsSource("reuters", reutersURL, "#reuters-news-col", "#reuters-twitter-col", 
+                            "<a class=\"twitter-timeline\" href=\"https://twitter.com/Reuters\">Tweets by Reuters</a>" + 
+                            "<script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>",
+                            "reuters-top-link");            
+
+var Breitbart = new NewsSource("breitbart", breitbartURL, "#breitbart-news-col", "#breitbart-twitter-col", 
+                                "<a class=\"twitter-timeline\" href=\"https://twitter.com/BreitbartNews\">Tweets by BreitbartNews</a>" + 
+                                "<script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>",
+                                "breitbart-top-link")                            
+
+display(CNN);
+display(HuffingtonPost);
+display(Reuters);
+display(Breitbart);
+
+
+function display(news) {
+    $.get(news.newsSourceURL, function (data) {
+        doEverything(news, data);
     });
 }
 
 
+function doEverything(news, data){
+    var article = data.articles[0];
+    var headline = article.title;
+    displayHeadline(headline, news.newsColID);
+    var author = article.author;
+    displayAuthor(author, news.newsColID);
+    var description = article.description;
+    displayDescription(description, news.newsColID);
+    var articleLink = article.url; var topLinkName = news.topArticleLink;
+    linkToArticle(articleLink, topLinkName, news.newsColID);
+    var picID = "\"" + news.source + "-pic\""; var imgSource = article.urlToImage;
+    displayImage(picID, imgSource, news.newsColID);
+    var dataHeight = " data-height=" + $("#".concat(news.source)).height();
+    //returns updated twitter HTML
+    news.embeddedTwitterHTML = formatAndDisplayTwitterTimeline(news.embeddedTwitterHTML, dataHeight, news.newsTwitterID);
+}
